0a8e306ded45dfa017598ee7a004eee948012333
bHabawel <rosshabawel12@gmail.com>
2023-12-11 20:31:02 +0800

    creating redux store and action creators

--- src/store.js
+++ src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -6,18 +8,20 @@ const initialState = {
 
 function reducer(state = initialState, action) {
   switch (action.type) {
-    case "account/balance":
-      return { ...state, balance: state.balance + action.balance };
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
     case "account/withdraw":
       return {
         ...state,
         balance: state.balance - action.payload,
       };
     case "account/requestLoan":
+      if (state.loan > 0) return state;
       return {
         ...state,
         loan: action.payload.loan,
         loanPurpose: action.payload.loanPurpose,
+        balance: state.balance + action.payload.loan,
       };
     case "account/payLoan":
       return {
@@ -30,3 +34,26 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+const store = createStore(reducer);
+
+function deposit(amount) {
+  return { type: "account/deposit", payload: 500 };
+}
+
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: 250 };
+}
+
+function requestLoan(amount) {
+  return {
+    type: "account/requestLoan",
+    payload: { loan: 1000, loanPurpose: "Buy a car" },
+  };
+}
+
+function payLoan(amount) {
+  return { type: "account/payLoan" };
+}
+
+store.dispatch(deposit(500));
